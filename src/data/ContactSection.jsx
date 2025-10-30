c07a4818eff50baaa7de5e1f2301916456960d99
Chandra Putera <Chandrawpsiloto@gmail.com>
2025-10-30 16:21:48 +0700

    Contact Section  dimaan pada section footer terdapat  content yang berisikan tentang pembuat web itu sendiri, about, contact, social dan lain lain

---
+++ src/data/ContactSection.jsx
@@ -0,0 +1,24 @@
+export const contactSection = [
+    {
+        content: `<h3>chandra-tera.me</h3>
+                        <p> Saya seorang Web Developer yang berfokus pada pengembangan aplikasi berbasis web 
+        dengan teknologi modern seperti Laravel, React, dan Tailwind CSS. 
+        Saya menyukai tantangan dalam menciptakan sistem yang efisien dan ramah pengguna.</p>`
+    },
+    {
+        content: `<h3>About</h3>
+                        <p>Kami adalah platform pembelajaran online yang berfokus pada pengembangan keterampilan digital, 
+        seperti pemrograman web, desain UI/UX, dan kecerdasan buatan. 
+        Dengan instruktur berpengalaman, kami membantu siswa belajar secara praktis dan berorientasi proyek.
+      </p></p>`
+    },
+    {
+        content: `<h3>Contact</h3>
+                        <p>by Email : <a href="mailto:chandrawp@gmail.com" target="_blank">chandrawpsiloto@gmail.com</a></p>
+                        <p>Kode Pos: 50714</p>`
+    },
+    {
+        content: `<h3>Social</h3>
+                        <p><b>Github: by </b><a href="https://github.com/ChandraTERA" target="_blank">Github-me</p>`
+    },
+]
