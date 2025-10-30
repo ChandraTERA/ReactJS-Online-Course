e98f8c07a43e702493f074f61c227a37de96b611
Chandra Putera <Chandrawpsiloto@gmail.com>
2025-10-30 16:36:20 +0700

    courses section adalah keterangan yang menjelaskan profile web yang membukakan course online yang hanya dapat di genggam saja

---
+++ src/data/CoursesSection.jsx
@@ -0,0 +1,13 @@
+export const coursesSection = {
+    content: `<p className="deskripsi">You Will Need This</p>
+                        <h2>Online Courses</h2>
+                        <p>An online course is a learning model that utilizes the internet to deliver educational materials and interactive content. 
+                        This method allows learners to access lessons, assignments, and discussions from anywhere and at any time without the need for face-to-face meetings.
+                        <br><br>
+                        In online courses, learners typically engage with video lectures, reading materials, quizzes, and interactive forums that support communication between instructors and learners. This flexible learning environment allows individuals to learn at their own pace, 
+                        manage their own schedules, and effectively acquire new knowledge or skills. 
+                        <br><br>
+                        Online courses can cover a wide range of topics—from academic subjects and professional training to creative and technical skills—making them an accessible and inclusive form of education for people around the world.</p>
+                        `,
+    image: 'https://img.freepik.com/free-vector/online-learning-isometric-concept_1284-17947.jpg?size=626&ext=jpg&ga=GA1.2.1769275626.1605867161'
+}
