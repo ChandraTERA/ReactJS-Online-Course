d966d44f4f782c62032ce50d1fecbc6b8637727b
Chandra Putera <Chandrawpsiloto@gmail.com>
2025-11-05 23:41:52 +0700

    Homesection.jsx

--- src/data/HomeSection.jsx
+++ src/data/HomeSection.jsx
@@ -1,7 +1,9 @@
 export const homeSection = {
+    
     image: 'https://img.freepik.com/free-vector/web-development-programmer-engineering-coding-website-augmented-reality-interface-screens-developer-project-engineer-programming-software-application-design-cartoon-illustration_107791-3863.jpg?size=626&ext=jpg&ga=GA1.2.1769275626.1605867161',
     content: `<p className="deskripsi">Belajar Programming #dirumahaja</p>
                         <h2>Tetap Sehat, Tetap Semangat</h2>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt, nobis.</p>
+                        <p>Online courses are an innovative form of modern learning that utilizes the internet as the primary medium for delivering material. Through this system, students can access various lessons, modules, learning videos, and interactive exercises without the constraints of time and space. This makes online courses an effective solution for providing flexible, efficient, and affordable education for all.</p>
+                        <p>The main advantage of online courses lies in their flexibility. Each participant is free to choose their own study time according to their personal schedule, making this method ideal for students, college students, and working professionals who want to improve their competencies without disrupting their primary activities. Furthermore, this learning system also provides participants with the opportunity to study independently at a pace that suits their individual abilities.</p>
                         <p><a href="" className="tbl-pink">Pelajari Lebih Lanjut</a></p>`
 }
